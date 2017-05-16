88a6ce4797a0eb3dcbcaeb1c60826959976d9f4d
Roman Shchekin <shchekin@nsd.ru>
2017-05-16 16:23:19 +0300

    Небольшие улучшения.

--- CS/Program.cs
+++ CS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using ClosedXML.Excel;
 
@@ -10,37 +11,46 @@ namespace ApiSample
 {
     class Program
     {
-        public const string ApiUrl = "http://0.0.0.0/api/get/securities?limit=50&skip=20150"; 
-        public static HashSet<string> SetOfIsins = new HashSet<string>(new[] { "XS0342241295", "RU000A0JS9F8" });
+        public const string ApiUrl = "https://nsddata.ru/api/get/securities";
+        private const string ApiKey = "<INSERT_KEY>";
         public const string FileName = "coupons.xlsx";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Загрузка данных...");
             var wc = new WebClient() { Encoding = Encoding.UTF8, };
-            var json = wc.DownloadString(ApiUrl);
+
+            var apiFilter = new JObject {
+                {
+                    "isin", new JObject
+                    {
+                        {"$in", new JArray { "RU000A0GKZJ8", "RU000A0JPKW6", "RU0001707002", "RU0002867854"}}
+                    }
+                }
+            };
+
+            var requestUrl = $"{ApiUrl}?filter={apiFilter.ToString(Formatting.None)}&apikey={ApiKey}";
+            var json = wc.DownloadString(requestUrl);
 
             var bonds = JArray.Parse(json);
-            var filteredBonds = bonds.Where(b => SetOfIsins.Contains((string)b["isin"])).ToList();
-
             Console.WriteLine("Формирование Excel...");
-            GenerateExcel(filteredBonds);
+            GenerateExcel(bonds);
             Console.WriteLine("Готово!");
         }
 
-        private static void GenerateExcel(List<JToken> input)
+        private static void GenerateExcel(JArray input)
         {
             var workbook = new XLWorkbook();
             workbook.Use1904DateSystem = true;
             var ws = workbook.Worksheets.Add("Coupons info");
 
             ws.Column(1).Width = 16;
-            ws.Column(2).Width = 70;
-            ws.Column(3).Width = 50;
-            ws.Column(4).Width = 35;
-            ws.Column(5).Width = 16;
+            ws.Column(2).Width = 65;
+            ws.Column(3).Width = 30;
+            ws.Column(4).Width = 16;
+            ws.Column(5).Width = 10;
 
-            var columns = new[] { "ISIN", "Название", "Эмитент", "Корпоративное действие", "Дата" };
+            var columns = new[] { "ISIN", "Эмитент", "Корпоративное действие", "Дата", "Размер" };
             foreach (var j in Enumerable.Range(0, columns.Length))
             {
                 ws.Cell(1, j + 1).Value = columns[j];
@@ -57,8 +67,16 @@ namespace ApiSample
                 foreach (var c in corpActions)
                     try
                     {
-                        data.Add(new List<JToken>() { bond["isin"], bond["name_full"], bond["issuer"]["name_full"],
-                            c["corp_action_type"]["name"], c["action_date_plan"] });
+                        if (c["corp_action_type"]["name_en"].ToString() == "Interest Payment")
+                        {
+                            data.Add(new List<JToken>() { bond["isin"], bond["issuer"]["name_full"],
+                            c["corp_action_type"]["name"], c["action_date_plan"], c["coupon"]["size"] });
+                        }
+                        else if (c["corp_action_type"]["name_en"].ToString() == "Principal repayment")
+                        {
+                            data.Add(new List<JToken>() { bond["isin"], bond["issuer"]["name_full"],
+                            c["corp_action_type"]["name"], c["action_date_plan"], c["repayment"]["size_cur"] });
+                        }
                     } catch (Exception ex)
                     {
                         Console.WriteLine(ex);
